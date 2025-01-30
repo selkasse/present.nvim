50a8cde320977e6a76494fa2332d434132b77d04
Sharif Elkassed <selkasse@gmu.edu>
2025-01-30 13:49:10 -0500

    add resize logic

--- lua/present.lua
+++ lua/present.lua
@@ -51,20 +51,12 @@ local parse_slides = function(lines)
   return slides
 end
 
-M.start_presentation = function(opts)
-  opts = opts or {}
-  opts.bufnr = opts.bufnr or 0
-  local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false) -- get all lines in the buffer
-
-  ---@type present.Slides
-  local parsed = parse_slides(lines)
-
-  ---@type vim.api.keyset.win_config[]
+local create_window_configurations = function()
   local width = vim.o.columns
   local height = vim.o.lines
 
+  return {
 
-  local windows = {
     background = {
       relative = 'editor',
       width = width,
@@ -95,7 +87,18 @@ M.start_presentation = function(opts)
     },
     -- footer = {}
   }
+end
 
+M.start_presentation = function(opts)
+  opts = opts or {}
+  opts.bufnr = opts.bufnr or 0
+
+  local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false) -- get all lines in the buffer
+  ---@type present.Slides
+  local parsed = parse_slides(lines)
+  local current_slide = 1
+
+  local windows = create_window_configurations()
   local background_float = create_floating_window(windows.background)
   local header_float = create_floating_window(windows.header)
   local body_float = create_floating_window(windows.body)
@@ -106,6 +109,7 @@ M.start_presentation = function(opts)
   local set_slide_content = function(idx)
     local slide = parsed.slides[idx]
 
+    local width = vim.o.columns
     local padding = string.rep(' ', (width - #slide.title) / 2)
     local title = padding .. slide.title
     vim.api.nvim_buf_set_lines(header_float.buf, 0, -1, false, { title })
@@ -113,7 +117,6 @@ M.start_presentation = function(opts)
   end
 
   -- move slides forward and back
-  local current_slide = 1
   vim.keymap.set('n', 'n', function()
     current_slide = math.min(current_slide + 1, #parsed.slides)
     set_slide_content(current_slide)
@@ -153,6 +156,25 @@ M.start_presentation = function(opts)
       -- therefore, we need to explicitly close the other buffers
       pcall(vim.api.nvim_win_close, header_float.win, true)
       pcall(vim.api.nvim_win_close, background_float.win, true)
+    end
+  })
+
+  -- Recalculate presentation configuration on resize
+  vim.api.nvim_create_autocmd('VimResized', {
+    group = vim.api.nvim_create_augroup('present-resized', {}),
+    callback = function()
+      if not vim.api.nvim_win_is_valid(body_float.win) or body_float.win == nil then
+        print('returning from resize callback due to invalid window')
+        return
+      end
+      print('valid window, proceeding to resize..')
+
+      local updated = create_window_configurations()
+      vim.api.nvim_win_set_config(header_float.win, updated.header)
+      vim.api.nvim_win_set_config(background_float.win, updated.background)
+      vim.api.nvim_win_set_config(body_float.win, updated.body)
+      -- Recalculates current slide contents
+      set_slide_content(current_slide)
     end
   })
 
